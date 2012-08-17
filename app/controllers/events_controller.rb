d35f629afa5b8658117e1a975b4b23c87f2173e5
Mike Butsko <butsko.7@osu.edu>
2012-08-17 08:34:50 -0400

    Changed to actioncontroller::metal

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,18 @@
-class EventsController < ApplicationController
+class EventsController < ActionController::Metal
+  include ActionController::Rendering
 
-  skip_before_filter :verify_authenticity_token
+  #skip_before_filter :verify_authenticity_token
 
   def create
     measureable = Measureable.find_or_create_by_name(params[:name], :service => params[:service])
     event = measureable.events.build(:timestamp => params[:timestamp])
     if event.save
-      render :nothing => true, :status => 201
+      #render :nothing => true, :status => 201
+      self.response_body = ""
+      self.status = 201
     else
-      render :text => event.errors.full_messages.join("\n"), :status => 400
+      self.response_body = event.errors.full_messages.join("\n")
+      self.status = 400
     end
   end
 
