f1fae2bb1bfae5e5f07853951ab18fb0f654ed93
mikegee <gee.24@osu.edu>
2012-08-16 21:36:08 -0400

    try ActionController::Metal

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,19 @@
-class EventsController < ApplicationController
-
-  skip_before_filter :verify_authenticity_token
+class EventsController < ActionController::Metal
 
   def create
     measureable = Measureable.find_or_create_by_name(params[:name], :service => params[:service])
     event = measureable.events.build(:timestamp => params[:timestamp])
     if event.save
-      render :nothing => true, :status => 201
+      self.response_body = ''
+      self.status = 201
     else
-      render :text => event.errors.full_messages.join("\n"), :status => 400
+      self.response_body = event.errors.full_messages.join("\n")
+      self.status = 400
     end
   end
 
   def index
-    render :text => Event.count
+    self.response_body = Event.count
   end
 
 end
