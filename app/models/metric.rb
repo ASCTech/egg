114899cc7501fdb181b8c1af6cdcf5cb8c7993f6
mikegee <gee.24@osu.edu>
2012-09-06 08:37:36 -0400

    Metric.increment now just one UPDATE (for the common case)

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,16 +1,26 @@
 class Metric < ActiveRecord::Base
-  belongs_to :measureable
-  attr_accessible :count, :time_pointer, :measureable_id
-
   self.abstract_class = true
 
+  belongs_to :measureable
+
+  attr_accessible :count, :time_pointer, :measureable_id
+
   default_scope order ("time_pointer DESC")
+
 
   TIME_SCALES = %w{ hourly daily weekly monthly }
 
   def self.increment(time_pointer, measureable_id)
-    metric = find_or_create_by_time_pointer_and_measureable_id(time_pointer, measureable_id)
-    metric.update_column(:count, metric.count + 1)
+    attrs = {
+      :time_pointer => time_pointer,
+      :measureable_id => measureable_id
+    }
+    rows_updated = where(attrs).update_all('count = count + 1')
+    case rows_updated
+    when 0; create!(attrs.merge(:count => 1))
+    when 1; return
+    else;   raise 'Multiple Metrics updated, not good!'
+    end
   end
 
   def self.[](time_scale)
